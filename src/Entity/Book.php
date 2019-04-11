0eb6a9ceb3a39921df89fa169897b11ef5846185
Fanny Rottee <frottee@tea-ebook.com>
2019-04-11 18:03:32 +0200

    Add entities

---
+++ src/Entity/Book.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $purchaseDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $readingDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $yearOfPublication;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isShortStory;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Format")
+     * @ORM\JoinColumn(name="format_id", referencedColumnName="id", nullable=false)
+     */
+    private $format;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getPurchaseDate(): ?\DateTimeInterface
+    {
+        return $this->purchaseDate;
+    }
+
+    public function setPurchaseDate(?\DateTimeInterface $purchaseDate)
+    {
+        $this->purchaseDate = $purchaseDate;
+    }
+
+    public function getReadingDate(): ?\DateTimeInterface
+    {
+        return $this->readingDate;
+    }
+
+    public function setReadingDate(?\DateTimeInterface $readingDate)
+    {
+        $this->readingDate = $readingDate;
+    }
+
+    public function getYearOfPublication(): ?\DateTimeInterface
+    {
+        return $this->yearOfPublication;
+    }
+
+    public function setYearOfPublication(?\DateTimeInterface $yearOfPublication)
+    {
+        $this->yearOfPublication = $yearOfPublication;
+    }
+}
